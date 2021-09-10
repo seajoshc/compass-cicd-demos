75eaaafc0e23ce485bcf40cba360cb8ed9fc0ea6
Josh Campbell <josh@p4a.net>
2021-09-09 22:05:40 -0700

    adds some fun :party:

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
+import random
+import time
 
-print("noice")
+weeeee = random.choice([True, False])
+
+if weeeee:
+    time.sleep(random.randint(2, 5))
+    print("noice")
+    exit(0)
+else:
+    print("nahhhh")
+    exit(69)
